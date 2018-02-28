d83ddd5f9f2732c6a85f5cbc904ecb76f8de6f9c
Piecuch Patryk <33668858+Piecuch-Patryk@users.noreply.github.com>
2018-02-28 17:53:00 +0000

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if(!isset($_SESSION['logged'])){
-    header('location: ./index.php');
-} else{
-    unset($_SESSION['logged']);
-    header('location: ./index.php');
-    }
-?>
