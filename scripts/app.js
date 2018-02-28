fedeaf3e1357ca1270f2593fc05e534859890c80
Piecuch Patryk <33668858+Piecuch-Patryk@users.noreply.github.com>
2018-02-28 18:38:49 +0000

    Run task manager app.
    Load tasks from database or display message if there is not any available.
    Listen form submit (login).

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+// get personalised tasks from data base;
+document.addEventListener('DOMContentLoaded', allTasks);
+// new task;
+document.getElementById('new').addEventListener('mouseup', newTask);
+//reset current task;
+document.getElementById('reset').addEventListener('mouseup', resetFaultField);
+document.getElementById('reset').addEventListener('mouseup', resetInputBgc);
+// listener for task validity buttons;
+document.addEventListener('DOMContentLoaded', eventListenerArray);
