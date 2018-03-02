340ad0bfbf3886a923f8a40bd8fe065f44ec1c7c
Piecuch Patryk <33668858+Piecuch-Patryk@users.noreply.github.com>
2018-03-02 10:09:36 +0000

    Added footer

--- logged-in/index.php
+++ logged-in/index.php
@@ -74,6 +74,8 @@ header('location: ./index.php');
        
     <!--end app container -->
     </main>
+    <!--footer-->
+    <?php include('../footer.php'); ?>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
     <!--app modules-->
