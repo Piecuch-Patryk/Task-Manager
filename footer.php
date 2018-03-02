17701f5bc88ae97ad1699ad1a5115bfc3a86f3a9
Piecuch Patryk <33668858+Piecuch-Patryk@users.noreply.github.com>
2018-03-02 10:09:01 +0000

    Footer

---
+++ footer.php
@@ -0,0 +1,7 @@
+<?php
+echo '<footer class="container-fluid bg-dark">
+        <div class="row py-2 text-light">
+            <p class="col-6 m-0 p-0 text-right">Project and Design</p><p class="col-6 m-0 p-0"><a class="p-2" href="https://piecuch-patryk.com" title="author">Patryk Piecuch</a></p>
+        </div>
+    </footer>';
+?>
